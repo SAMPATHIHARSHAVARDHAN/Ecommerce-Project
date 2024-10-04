fdb753704ffabbf193b28d564ba0feacf1c77da5
kunchala1971 <kunchala1971@gmail.com>
2024-10-04 18:11:49 +0530

    Describe the modifications you made

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from models import product
+from .models import Post
+
 # Register your models here.
-class productAdmin(admin.ModelAdmin):
-    list_display=('id','product_id','product_description','product_type','price','discount','points','gst')
-admin.site.register(product,productAdmin)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('product_id','product_description','product_type','price','discount','points','gst')
+    search_fields=('title','content')
+
+
+admin.site.register(Post , PostAdmin)
--- products/models.py
+++ products/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class product(models.Models):
-    product_id=models.IntegerField(max_length=150)
-    product_name=models.CharFieldField(max_length=150)
-    product_description=models.CharFieldField(max_length=150)
-    product_type=models.CharField(max_length=150)
-    price=models.IntegerField(max_length=50)
-    discount=models.IntegerField(max_length=50)
-    gst=models.IntegerField(max_length=50)
+class Post(models.Model):
+    product_id=models.IntegerField(default=0)
+    product_description=models.CharFieldField(default="null")
+    product_type=models.CharField(max_length=150,default="null")
+    price=models.FloatField(max_length=50,default=0.00)
+    discount=models.FloatField(max_length=50,default=0.00)
+    gst=models.FloatField(max_length=50,default=0.00)
+    points=models.IntegerField(default=0)
     
-class Meta:
-    verbose_nmae="product"
-    verbose_name_plural="products"
+
     
     
 
